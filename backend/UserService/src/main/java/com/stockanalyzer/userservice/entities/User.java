df96c7874f73aaf0ec4a5487ff52bad679b4c503
vamsi krishna <chavavamsi29@gmail.com>
2025-12-28 09:42:35 +0530

    Adding entity and table details

--- backend/UserService/src/main/java/com/stockanalyzer/userservice/entities/User.java
+++ backend/UserService/src/main/java/com/stockanalyzer/userservice/entities/User.java
@@ -2,10 +2,14 @@ package com.stockanalyzer.userservice.entities;
 
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name="users")
 public class User {
 	
 	@Id
